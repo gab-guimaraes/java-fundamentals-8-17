c9fe42aea5a5a2c87c91268b9ca296cb5255eca5
Gabriel Guimaraes <dedito182@gmail.com>
2022-01-06 17:47:34 -0300

    collections

--- src/com/johnwick182/Collections.java
+++ src/com/johnwick182/Collections.java
@@ -1,4 +1,9 @@
 package com.johnwick182;
 
 public class Collections {
+    //collection - root collections, set, queue and list are derivatives
+    //set - colletion that don't have duplicated data
+    //list - list with duplicated data
+    //map - key and value without duplicated data
+    //fila - first in first out
 }
